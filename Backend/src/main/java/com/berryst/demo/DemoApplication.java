07c985449f6acc761e58f6fdff742f9fb7a58b73
Shirui Cheng <shiruic@outlook.com>
2021-09-11 21:14:57 +0800

    Modify Feedback parameter structure (to JSON)

--- Backend/src/main/java/com/berryst/demo/DemoApplication.java
+++ Backend/src/main/java/com/berryst/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.berryst.demo;
 
+import org.apache.ibatis.javassist.compiler.ast.Pair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class DemoApplication {
