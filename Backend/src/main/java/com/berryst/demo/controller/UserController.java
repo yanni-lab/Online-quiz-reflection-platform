4370fd90c1a044b2551b576c536f17e7556b5bb5
ginkin <hansun1@student.unimelb.edu.au>
2021-09-12 20:48:18 +1000

    resolve CORS problem change the way of controller receiving parameter

--- Backend/src/main/java/com/berryst/demo/controller/UserController.java
+++ Backend/src/main/java/com/berryst/demo/controller/UserController.java
@@ -3,23 +3,36 @@ package com.berryst.demo.controller;
 import com.berryst.demo.DemoApplication;
 import com.berryst.demo.model.User;
 import com.berryst.demo.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value="/user")
+@CrossOrigin
 public class UserController {
     @Resource
     private UserService userService;
 
 
-    @RequestMapping(value="/register",method= RequestMethod.GET)
-    public String register(boolean isSupervisor, String username, String password, String email){
+    @RequestMapping(value="/register",method= RequestMethod.POST)
+    public String register(@RequestBody String data, HttpServletResponse response) throws JSONException {
+        JSONObject receivedData = new JSONObject(data);
+        String username = receivedData.getString("username");
+        String password = receivedData.getString("password");
+        String email = receivedData.getString("email");
+        boolean isSupervisor = receivedData.getBoolean("isSupervisor");
+
+
         User user = new User(1, username, password, email,isSupervisor);
         User exist_u = userService.queryUserByEmail(email);
         if (exist_u == null){
@@ -28,6 +41,7 @@ public class UserController {
             Timestamp curTime = new Timestamp(new Date().getTime());
             String token = curUser.getUserId()+":"+curTime.getTime();
             DemoApplication.tokenList.put(curUser.getUserId(), curTime.getTime());
+            System.out.println(token);
             return token;//succeed
         }
         else{
@@ -35,18 +49,26 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value="/login",method= RequestMethod.GET)
-    public String login(String username, String password){
+    @RequestMapping(value="/login",method= RequestMethod.POST)
+    public HashMap<String, String> login(@RequestBody String data, HttpServletResponse response) throws JSONException {
+        JSONObject receivedData = new JSONObject(data);
+
+        String username = receivedData.getString("username");
+        String password = receivedData.getString("password");
+
+        HashMap<String, String> resultMap = new HashMap<String, String>();
         User exist_u = userService.queryUserByUsername(username);
         if (exist_u.getPassword().equals(password)){
             Timestamp curTime = new Timestamp(new Date().getTime());
             String token = exist_u.getUserId()+":"+curTime.getTime();
             DemoApplication.tokenList.remove(exist_u.getUserId());
             DemoApplication.tokenList.put(exist_u.getUserId(), curTime.getTime());
-            return token;//succeed
+            resultMap.put("token",token);
+            return resultMap;//succeed
         }
         else{
-            return "Wrong username or password!";//log in failed
+            resultMap.put("token",null);
+            return resultMap;//log in failed
         }
     }
 }
