6e0d188f95cdf2c9f2220db67b2b0c3e53299954
ginkin <hansun1@student.unimelb.edu.au>
2021-09-16 05:08:11 +1000

    quizController output format change

--- Backend/src/main/java/com/berryst/demo/controller/QuizController.java
+++ Backend/src/main/java/com/berryst/demo/controller/QuizController.java
@@ -2,6 +2,7 @@ package com.berryst.demo.controller;
 
 import com.berryst.demo.model.Quiz;
 import com.berryst.demo.service.QuizService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value="/service")
@@ -22,21 +25,26 @@ public class QuizController {
     private QuizService quizService;
 
     @RequestMapping(value="/available_quiz",method= RequestMethod.POST)
-    public ObjectNode getPublicQuiz(@RequestBody String data, HttpServletResponse response) {
+    public ObjectNode getPublicQuiz(@RequestBody String data, HttpServletResponse response) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectNode node = objectMapper.createObjectNode();
-        node.put("quizList", quizService.getPublicQuizList().toString());
+        String quizList = objectMapper.writeValueAsString(quizService.getPublicQuizList());
+        node.put("quizList", quizList);
         node.put("errorCode",1);
         return node;
     }
 
     @RequestMapping(value="/quiz_content",method= RequestMethod.POST)
-    public ObjectNode getQuizContent(@RequestBody String data, HttpServletResponse response) throws JSONException {
+    public ObjectNode getQuizContent(@RequestBody String data, HttpServletResponse response) throws JSONException, JsonProcessingException {
         JSONObject receivedData = new JSONObject(data);
         int quizId = receivedData.getInt("quizId");
 
         ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode node = objectMapper.convertValue(quizService.getQuizContent(quizId), ObjectNode.class);
+        Quiz thequiz = quizService.getQuizContent(quizId);
+        System.out.println(Arrays.toString(thequiz.getFeedback().split("\n")));
+        ObjectNode node = objectMapper.convertValue(thequiz, ObjectNode.class);
+
+        node.put("feedback", thequiz.getFeedback().replace("###","\\n"));
         node.put("errorCode",1);
 
         return node;
