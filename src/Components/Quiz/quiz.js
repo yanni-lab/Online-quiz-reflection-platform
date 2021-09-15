b5b24fd86ec7dc5fe9de019b3f9ea034bc645a73
BillyWang817 <70014147+BillyWang817@users.noreply.github.com>
2021-09-16 02:34:22 +1000

    Update to Quiz.js and Quiz.css

--- src/Components/Quiz/quiz.js
+++ src/Components/Quiz/quiz.js
@@ -1,166 +1,146 @@
 import React,{ Component } from 'react';
 import './Quiz.css';
+import data from "../../quiz.json"
+import {Row, Col, Button,Form,Modal} from 'react-bootstrap';
 
-import {Row, Col, Button,Form} from 'react-bootstrap';
-import {Link} from "react-router-dom";
 
 
 class Quiz extends React.Component {
     constructor(props){
         super(props);
-
         //获取题目详情
-        this.quizList =  {
-            'quiz':[
-                {
-                    "quizNum":1,
-                    "quizQuestion":'你掉的是这个金斧子还是银斧子？',
-                    "quizOptions":[
-                        {'option':'金斧子'},
-                        {'option':'银斧子'},
-                        {'option':'我没掉斧子'}
-                        ]
-
-
-                },
-                {
-                    "quizNum":2,
-                    "quizQuestion":'When the group needs suggestions, I...',
-                    "quizOptions":[
-                        {'option':'Do not make suggestions'},
-                        {'option':'Tell the group what to do'},
-                        {'option':'Discuss my suggestions with the group'},
-                        {'option':"Make sure everyone's suggestions are heard" }
-                        ]
-                },
-                {
-                    "quizNum":3,
-                    "quizQuestion":'123',
-                    "quizOptions":[
-                        {'option':'abc'},
-                        {'option':'def'},
-                        {'option':'ghi'},
-                        {'option':"lmn" }
-                    ]
-                },
-                {
-                    "quizNum":4,
-                    "quizQuestion":'',
-                    "quizOptions":[
-
-                    ]
-                }
-            ]
-        }
-
-
-
+        this.quizList = data.questions
+        this.quizList.push(
+            {
+                "question":'Congrats! You have reached the end!',
+                "choices": [
+                ]
+            }
+        )
 
         this.state = {
             currQuestion:0,
-            value:'',
+            value:0,
             options:[],
             question:'',
             displayFormer : 'none',
             displayNext: 'none',
-            showFeedbackButton: 'none'
+            showFeedbackButton: 'none',
+            leave:false
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.nextClick = this.nextClick.bind(this);
         this.formerClick = this.formerClick.bind(this);
+        this.leaveQuiz = this.leaveQuiz.bind(this);
+        this.cancelLeaveQuiz = this.cancelLeaveQuiz.bind(this);
 
 
     };
 
+
     handleChange(event) {
         this.state.value = event.target.value;
-        console.log('选择了'+this.state.value);
+        console.log('该选项得分为'+this.state.value);
         this.setState({
             displayNext: 'block',
-            //displayFormer: this.state.currQuestion>0 ? 'none': 'block',
 
         })
     }
 
 
     nextClick(){
-        //this.state.currQuestion+=1
+        this.state.currQuestion+=1
+
+
         this.setState({
-            currQuestion:this.state.currQuestion+1,
+            //currQuestion:this.state.currQuestion+1,
             displayFormer: this.state.currQuestion>0 ? 'block': 'none',
             displayNext: 'none',
-            showFeedbackButton: this.state.currQuestion==2 ? 'block': 'none'
+            showFeedbackButton: this.state.currQuestion== this.quizList.length-1? 'block': 'none'
 
+        })
+        console.log(this.state)
+        console.log(this.quizList)
+
+    }
+
+    formerClick(){
+        this.state.currQuestion-=1
+
+        this.setState({
+
+            displayFormer: this.state.currQuestion>0 ? 'block': 'none',
+            showFeedbackButton: this.state.currQuestion== this.quizList.length-1? 'block': 'none'
 
 
         })
         console.log(this.state)
-        //console.log(this.state)
-        // if(this.state.currQuestion>0){
-        //     this.setState({displayFormer:true})
-        // }
+
     }
 
-    formerClick(){
-        //this.state.currQuestion-=1
+    leaveQuiz(){
         this.setState({
-            currQuestion:this.state.currQuestion-1,
-            displayFormer: this.state.currQuestion>0 ? 'block': 'none',
-            //displayNext: 'none'
+            leave: true,
 
         })
+
     }
 
+    cancelLeaveQuiz(){
+        this.setState({
+            leave: false
+        })
 
+    }
 
 
 
 
 
     render(){
-        const quizList =  {
-            'quiz':[
-                {
-                    "quizNum":1,
-                    "quizQuestion":'你掉的是这个金斧子还是银斧子？',
-                    "quizOptions":[
-                        {'option':'金斧子'},
-                        {'option':'银斧子'},
-                        {'option':'我没掉斧子'}
-                    ]
-
-
-                },
-                {
-                    "quizNum":2,
-                    "quizQuestion":'When the group needs suggestions, I...',
-                    "quizOptions":[
-                        {'option':'Do not make suggestions'},
-                        {'option':'Tell the group what to do'},
-                        {'option':'Discuss my suggestions with the group'},
-                        {'option':"Make sure everyone's suggestions are heard" }
-                    ]
-                },
-                {
-                    "quizNum":3,
-                    "quizQuestion":'123',
-                    "quizOptions":[
-                        {'option':'abc'},
-                        {'option':'def'},
-                        {'option':'ghi'},
-                        {'option':"lmn" }
-                    ]
-                },
-                {
-                    "quizNum":4,
-                    "quizQuestion":'Congrats! You have reached the end!',
-                    "quizOptions":[
-
-                    ]
-                }
-            ]
-        }
+        // const quizList =  {
+        //     'quiz':[
+        //         {
+        //             "quizNum":1,
+        //             "quizQuestion":'你掉的是这个金斧子还是银斧子？',
+        //             "quizOptions":[
+        //                 {'option':'金斧子'},
+        //                 {'option':'银斧子'},
+        //                 {'option':'我没掉斧子'}
+        //             ]
+        //
+        //
+        //         },
+        //         {
+        //             "quizNum":2,
+        //             "quizQuestion":'When the group needs suggestions, I...',
+        //             "quizOptions":[
+        //                 {'option':'Do not make suggestions'},
+        //                 {'option':'Tell the group what to do'},
+        //                 {'option':'Discuss my suggestions with the group'},
+        //                 {'option':"Make sure everyone's suggestions are heard" }
+        //             ]
+        //         },
+        //         {
+        //             "quizNum":3,
+        //             "quizQuestion":'123',
+        //             "quizOptions":[
+        //                 {'option':'abc'},
+        //                 {'option':'def'},
+        //                 {'option':'ghi'},
+        //                 {'option':"lmn" }
+        //             ]
+        //         },
+        //         {
+        //             "quizNum":4,
+        //             "quizQuestion":'Congrats! You have reached the end!',
+        //             "quizOptions":[
+        //
+        //             ]
+        //         }
+        //     ]
+        // }
 
 
 
@@ -170,28 +150,29 @@ class Quiz extends React.Component {
             <div className="quizPage">
                 <Row>
                     <Col>
-                        <Link to='/listQuiz'>
+
                             <Button className="backButton"
                                     size="lg"
                                     variant="outline-primary"
+                                    onClick = {this.leaveQuiz}
 
                             >Exit Quiz</Button>
-                        </Link>
+
                     </Col>
                 </Row>
 
                 <div className="box justify-content-center align-items-center">
                     <Form className="quizForm">
                         <div className = "question">
-                            {quizList.quiz[this.state.currQuestion].quizQuestion}
+                            {this.quizList[this.state.currQuestion].question}
                         </div>
 
 
                         <form>
                             {
-                                quizList.quiz[this.state.currQuestion].quizOptions.map((option) =>
+                                this.quizList[this.state.currQuestion].choices.map((option) =>
                                     <div className="option">
-                                        <Button className="optionButton" value={option.option} onClick={this.handleChange}>{option.option}</Button>
+                                        <Button className="optionButton" value={option.score} onClick={this.handleChange}>{option.choice}</Button>
                                     </div>)
 
                             }
@@ -212,28 +193,56 @@ class Quiz extends React.Component {
                 </div>
 
 
-                <Row className = "formerNextButtonRow">
-                    <Col>
-                        <div style={{display: this.state.displayFormer}}>
-                            <Button className="formerButton"
-                                    size="lg"
-                                    variant="outline-primary"
-                                    onClick = {this.formerClick}
-                            >
-                                &lt; </Button>
-                        </div>
-                    </Col>
+                <div  className = "formerNextButtonRow">
+                    <Row>
+                        <Col>
+                            <div style={{display: this.state.displayFormer}}>
+                                <Button className="formerButton"
+                                        size="lg"
+                                        variant="outline-primary"
+                                        onClick = {this.formerClick}
+                                >
+                                    &lt; </Button>
+                            </div>
+                        </Col>
 
-                    <Col>
-                        <div style={{display: this.state.displayNext}}>
-                            <Button className="nextButton"
-                                    size="lg"
-                                    variant="outline-primary"
-                                    onClick = {this.nextClick}
-                            >  &gt; </Button>
-                        </div>
-                    </Col>
-                </Row>
+                        <Col>
+                            <div style={{display: this.state.displayNext}}>
+                                <Button className="nextButton"
+                                        size="lg"
+                                        variant="outline-primary"
+                                        onClick = {this.nextClick}
+                                >  &gt; </Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+
+
+                <Modal  show = {this.state.leave}
+                        onClick =  {this.cancelLeaveQuiz}
+                >
+
+                    <Modal.Body>
+                        Are you sure you want to exit the quiz? (Have you saved / shared ?)
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button href = "./listQuiz" className = "ensureExit">Yes</Button>
+                        <Button onClick = {this.cancelLeaveQuiz} className = "cancelExit">No</Button>
+                    </Modal.Footer>
+
+
+                </Modal>
+
+
+
+
+
+
+
+
+
+
 
             </div>
 
